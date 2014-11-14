6160cf409d106a632fa1e12de40558b5d14a2bd0
zhangheng <zhhkay@gmail.com>
2014-11-14 22:36:39 +0800

    up, work in progress

--- src/Plugins/org.vtt.myplugin/src/internal/CtManage.cpp
+++ src/Plugins/org.vtt.myplugin/src/internal/CtManage.cpp
@@ -13,8 +13,6 @@
 #include <berryIViewDescriptor.h>
 #include <mitkDataStorageEditorInput.h>
 
-const std::string CCtManage::VIEW_ID = "org.mitk.views.vttview";
-
 CCtManage::CCtManage(Ui::VttViewControls controls)
 {
 	m_Controls = controls;
@@ -24,7 +22,7 @@ CCtManage::CCtManage(Ui::VttViewControls controls)
 	connect(m_Controls.ButtonCTOpen, SIGNAL(clicked()), this, SLOT(OnButtonCTOpen()));
 	connect(m_Controls.ButtonCTImport, SIGNAL(clicked()), this, SLOT(OnButtonCTImport()));
 	connect(m_Controls.CTListWidget, SIGNAL(currentItemChanged(QListWidgetItem*,QListWidgetItem*)), this,
-			SLOT(OnCTListCurrentItem(QListWidgetItem*,QListWidgetItem*)));
+		SLOT(OnCTListCurrentItem(QListWidgetItem*,QListWidgetItem*)));
 
 	//test
 	m_Controls.CTListWidget->addItem(new QListWidgetItem(QIcon(":/org.vtt.myplugin/Load_48.png"), "111111"));
@@ -39,42 +37,29 @@ void CCtManage::OnCTListCurrentItem(QListWidgetItem* current, QListWidgetItem*)
 
 void CCtManage::OnButtonCTOpen()
 {
-	//org.mitk.editors.stdmultiwidget
-	berry::IWorkbenchPage::Pointer page = this->GetSite()->GetPage();
-	mitk::DataStorageEditorInput::Pointer input(new mitk::DataStorageEditorInput(GetDataStorageReference()));
-	page->OpenEditor(input,"org.mitk.editors.stdmultiwidget");
-
 	//this->GetRenderWindowPart(QmitkAbstractView::BRING_TO_FRONT | QmitkAbstractView::OPEN);
 
-//	berry::IWorkbench* currentWorkbench = berry::PlatformUI::GetWorkbench();
-//	berry::IWorkbenchWindow::Pointer currentWorkbenchWindow = currentWorkbench->GetActiveWorkbenchWindow();
-//	berry::IWorkbenchPage::Pointer currentPage = currentWorkbenchWindow->GetActivePage();
-//	//CtestEditor * view = new CtestEditor();
-//
-//	//currentPage->Activate();
-//	berry::IEditorPart::Pointer editor = currentPage->GetActiveEditor();
-//	berry::IWorkbenchPart::Pointer currentPart = currentPage->GetActivePart();
-//	MITK_INFO<< currentPart->GetSite()->GetPluginId();
+	//	berry::IWorkbench* currentWorkbench = berry::PlatformUI::GetWorkbench();
+	//	berry::IWorkbenchWindow::Pointer currentWorkbenchWindow = currentWorkbench->GetActiveWorkbenchWindow();
+	//	berry::IWorkbenchPage::Pointer currentPage = currentWorkbenchWindow->GetActivePage();
+	//	//CtestEditor * view = new CtestEditor();
+	//
+	//	//currentPage->Activate();
+	//	berry::IEditorPart::Pointer editor = currentPage->GetActiveEditor();
+	//	berry::IWorkbenchPart::Pointer currentPart = currentPage->GetActivePart();
+	//	MITK_INFO<< currentPart->GetSite()->GetPluginId();
 
-//打开帮助页面
-//berry::PlatformUI::GetWorkbench()->GetIntroManager()->ShowIntro(berry::PlatformUI::GetWorkbench()->GetActiveWorkbenchWindow(), false);
-//新增view页面，Perspective
-//berry::PlatformUI::GetWorkbench()->ShowPerspective("org.blueberry.perspectives.help", berry::PlatformUI::GetWorkbench()->GetActiveWorkbenchWindow());
-//QmitkAboutDialog* aboutDialog = new QmitkAboutDialog(QApplication::activeWindow(), NULL);
-//aboutDialog->open();
+	//打开帮助页面
+	//berry::PlatformUI::GetWorkbench()->GetIntroManager()->ShowIntro(berry::PlatformUI::GetWorkbench()->GetActiveWorkbenchWindow(), false);
+	//新增view页面，Perspective
+	//berry::PlatformUI::GetWorkbench()->ShowPerspective("org.blueberry.perspectives.help", berry::PlatformUI::GetWorkbench()->GetActiveWorkbenchWindow());
+	//QmitkAboutDialog* aboutDialog = new QmitkAboutDialog(QApplication::activeWindow(), NULL);
+	//aboutDialog->open();
 }
 
 void CCtManage::OnButtonCTImport()
 {
-QStringList fileNames = QFileDialog::getOpenFileNames(NULL, "load CT image", "", mitk::CoreObjectFactory::GetInstance()->GetFileExtensions());
-QString str = fileNames.join(",\n");
-MITK_INFO<< str.toStdString();
-}
-
-void CCtManage::CreateQtPartControl(QWidget *)
-{
-}
-
-void CCtManage::SetFocus()
-{
+	QStringList fileNames = QFileDialog::getOpenFileNames(NULL, "load CT image", "", mitk::CoreObjectFactory::GetInstance()->GetFileExtensions());
+	QString str = fileNames.join(",\n");
+	MITK_INFO<< str.toStdString();
 }
--- src/Plugins/org.vtt.myplugin/src/internal/CtManage.h
+++ src/Plugins/org.vtt.myplugin/src/internal/CtManage.h
@@ -1,6 +1,6 @@
 ﻿/*===================================================================
- CT管理类,
- ===================================================================*/
+CT管理类,
+===================================================================*/
 
 #include <berryISelectionListener.h>
 #include <QmitkAbstractView.h>
@@ -8,26 +8,16 @@
 
 #include <QmitkAbstractRenderEditor.h>
 
-class CtestEditor: public QmitkAbstractRenderEditor
+class CCtManage: public QObject
 {
-
-};
-
-class CCtManage: public QmitkAbstractView
-{
-Q_OBJECT
+	Q_OBJECT
 public:
 	static const std::string VIEW_ID;
 	CCtManage(Ui::VttViewControls controls);
-public slots:
-	void OnCTListCurrentItem(QListWidgetItem* current, QListWidgetItem* previous);
-	void OnButtonCTOpen();
-	void OnButtonCTImport();
-
+	public slots:
+		void OnCTListCurrentItem(QListWidgetItem* current, QListWidgetItem* previous);
+		void OnButtonCTOpen();
+		void OnButtonCTImport();
 protected:
-
-	virtual void CreateQtPartControl(QWidget *);
-
-	virtual void SetFocus();
 	Ui::VttViewControls m_Controls;
 };
